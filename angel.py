deefef24e0478309007f5d849d1ffbd68d7f234e
nanotechtg <link2nanotech@gmail.com>
2024-11-20 17:33:39 +0530

    Update angel.py

--- angel.py
+++ angel.py
@@ -13,10 +13,6 @@ bot = telebot.TeleBot(TOKEN)
 # Flask app
 app = Flask(__name__)
 
-@app.route('/')
-def health_check():
-    return "Angel Lol Healthy", 200
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=3000)
 
